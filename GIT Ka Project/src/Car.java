c0226760ac938d5990b0c5cc7cb2d0a98f1b1c4f
Chaitanya <Chaitanya@LAPTOP-K74000V7>
2021-08-03 17:40:17 +0530

    this is new car benz

---
+++ GIT Ka Project/src/Car.java
@@ -0,0 +1,15 @@
+
+public class Car {
+
+	public static void main(String[] args) {
+
+		Car c = new Car();
+		c.mercedes();
+		System.out.println("this is cars showroom");
+	}
+
+	public void mercedes() {
+
+		System.out.println("--------mercedes Benz--------");
+	}
+}
